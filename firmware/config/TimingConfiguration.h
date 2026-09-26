d1079e03d4e48b694bc8d840ad96cf262f685cd6
Topher Webb <topher2880@gmail.com>
2026-09-26 11:23:57 +0930

    Set flight display cycle to 10 seconds

--- firmware/config/TimingConfiguration.h
+++ firmware/config/TimingConfiguration.h
@@ -8,5 +8,5 @@ namespace TimingConfiguration
     static const uint32_t FETCH_INTERVAL_SECONDS = 30; // seconds
 
     // Display cycling configuration
-    static const uint32_t DISPLAY_CYCLE_SECONDS = 3; // seconds per flight when multiple flights
+    static const uint32_t DISPLAY_CYCLE_SECONDS = 10; // seconds per flight when multiple flights
 }
